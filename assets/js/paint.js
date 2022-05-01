410cfd5db89cd8f152c3e16472c4fe1aaea60431
Spencer Krull <spencerkrull138@gmail.com>
2022-04-30 23:09:56 -0600

    troubleshoot/fixed opacity not working

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -8,15 +8,15 @@ function configureListeners() {
 }
 
 function addOpacity(event) {
-    if (this.classList.contains('dim')){
+    if (!this.classList.contains('dim')){
         this.classList.add('dim')
-    }
+    } 
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
     if (this.classList.contains('dim')){
-        this.classList.remove('dim')
+        this.classList.remove('dim');
     }
 
     let element = document.getElementById('color-price');
@@ -49,28 +49,28 @@ function getProductInfo(paintColor) {
             updatePrice(colorName, price)  
             break;   
         case 'pn4':
-            price = '$4.99'
-            colorName = 'Solid Black'
+            price = '$13.42'
+            colorName = 'Solid Red'
             updatePrice(colorName, price)  
             break;   
         case 'pn5':
-            price = '$8.22'
-            colorName = 'Solid Cyan'
+            price = '$21.98'
+            colorName = 'Solid White'
             updatePrice(colorName, price)       
             break;   
         case 'pn6':
-            price = '$11.99'
-            colorName = 'Solid Purple'
+            price = '$4.99'
+            colorName = 'Solid Black'
             updatePrice(colorName, price)        
             break;   
         case 'pn7':
-            price = '$13.42'
-            colorName = 'Solid Red'
+            price = '$8.22'
+            colorName = 'Solid Cyan'
             updatePrice(colorName, price) 
             break;   
         case 'pn8':
-            price = '$21.98'
-            colorName = 'Solid White'
+            price = '$11.99'
+            colorName = 'Solid Purple'
             updatePrice(colorName, price)   
             break;   
         case 'pn9':
